653dc54492eecc808eccf9f5c8d3cea2ad43c41b
JT <jeka-tech@mail.ru>
2023-03-02 08:01:24 +0400

    Added module with LEGB-rule

---
+++ LEGB/main.py
@@ -0,0 +1,14 @@
+open = 'global'
+# max = 10
+def outer():
+    open = 'encoled'
+
+    def inner():
+        open = 'local'
+        print(open)
+        print(max([1,2,3]))
+
+    inner()
+
+if __name__ == '__main__':
+    outer()
