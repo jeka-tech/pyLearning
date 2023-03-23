a136e1228b3368627cb80f11d8484a19acb9ba67
Evgeniy <jeka-tech@mail.ru>
2023-03-23 23:59:29 +0400

    Added division function in calculator.py

--- calculator_module/calculator.py
+++ calculator_module/calculator.py
@@ -1,7 +1,8 @@
 def subtraction (first, second):
+    const = -1
+    temp_var = 0
+
     if second > 0:
-        const = -1
-        temp_var = 0
         for i in range(second):
             temp_var = temp_var + const
         second = temp_var
@@ -11,45 +12,68 @@ def subtraction (first, second):
 
 def multiplication (first, second):
     result = 0
-    for i in range(second):
-        result = result + first
+    if first == 0 or second == 0:
+        return 0
+    if second > 0:
+        for i in range(second):
+            result = result + first
+    elif second < 0:
+        for i in range(abs(second)):
+            result = subtraction(result, first)
     return result
 
 
-# def division (a: int, b: int):
-#     if b == 0:
-#         return "Division by zero"
-#     elif a == 0:
-#         return 0
-#     whole = 0
-#     while (a-b) >= 0:
-#         whole += 1
-#         a = a - b
-#     if a == 0:
-#         return whole
-#     else:
-#         fractional = 0
-#         a = a*10
-#         while (a - b) >= 0:
-#             fractional += 1
-#             a = a - b
-#         if a == 0:
-#             return whole + fractional/10
-#         print(whole + fractional/10)
-#
-# if __name__ == "__main__":
-#     assert (division(200, 5) == 40)
-#     assert (division(201, 5) == 40.2)
-#     assert (division(2, 4) == 0.5)
-#     assert (division(5, 5) == 1)
-#     assert (division(125, 5) == 25)
-#     assert (division(0, 5) == 0)
-#     assert (division(3, 7) == round(3/7, 16))
-#     assert (division(12343, 7443) == round(12343 / 7443, 16))
-#     assert (division(-6, 12) == -0.5)
-#     assert (division(-6, -12) == 0.5)
-#     assert (division(6, -12) == -0.5)
-#     assert (division(0, 0) == "Division by zero")
-#     assert (division(3, 0) == "Division by zero")
-#     print(division(2, 4))
-#
+def division (a, b):
+    if b == 0:
+        return "Division by zero"
+    elif a == 0:
+        return 0
+
+    if multiplication(a,b) > 0:
+        result = ""
+    else:
+        result = "-"
+
+    a = abs(a)
+    b = abs(b)
+
+    whole = 0
+
+    while (a-b) >= 0:
+        whole += 1
+        a = a - b
+    if a == 0:
+        return whole
+
+    result = result + str(whole) + '.'
+
+    for i in range(16):
+        fractional = 0
+        a = a*10
+        while (a - b) >= 0:
+            fractional += 1
+            a = a - b
+        result = result + str(fractional)
+        if a == 0:
+            return float(result)
+    return float(result)
+
+
+
+if __name__ == "__main__":
+    assert (division(200, 5) == 40)
+    assert (division(201, 5) == 40.2)
+    assert (division(2, 4) == 0.5)
+    assert (division(5, 5) == 1)
+    assert (division(125, 5) == 25)
+    assert (division(0, 5) == 0)
+    assert (division(3, 7) == round(3/7, 16))
+    assert (division(12343, 7443) == round(12343 / 7443, 16))
+    assert (division(-6, 12) == -0.5)
+    assert (division(-6, -12) == 0.5)
+    assert (division(6, -12) == -0.5)
+    assert (division(0, 0) == "Division by zero")
+    assert (division(3, 0) == "Division by zero")
+
+
+
