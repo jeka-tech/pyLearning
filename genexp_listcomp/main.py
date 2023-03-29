8dff14a9a6c055f3966c3ad78101ca145fa9bb68
Evgeniy <jeka-tech@mail.ru>
2023-03-29 23:09:16 +0400

    No comments

--- genexp_listcomp/main.py
+++ genexp_listcomp/main.py
@@ -1,7 +1,15 @@
+
+# list-comprehensions -> listcomps
+# generator-expressions -> genexp
+# Синтаксис одинаковый, но в genexp используются круглые скобки
+# Отличие big_numbers = [e for e in range(100000)]
+
 text = 'First second : third 12345'
 elements = text.split()
-suspend = (e for e in range(1000_000))
+Elements = [e.capitalize() for e in elements if "th" in e]
+ints = [-3, 6, 0, -2, 7, 9]
+positive = [e for e in ints if e > 0]
 
 if __name__ == '__main__':
-    print([e.capitalize() for e in elements])
-    print(1000_000)
+    big_numbers = [e for e in range(10_000_000)]
+    print(big_numbers.__len__())
