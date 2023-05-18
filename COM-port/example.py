9448dbbc1326783547d38de221dd725c536e8e9c
JT <jeka-tech@mail.ru>
2023-05-18 11:31:38 +0400

    Added a lesson about COM-port

---
+++ COM-port/example.py
@@ -0,0 +1,19 @@
+import serial
+import time
+
+s = serial.Serial('COM4', 115200)
+
+
+while True:
+    try:
+        if (s.isOpen() == False):
+            s.open()
+            print('Порт закрыт')
+        a = s.readline().decode('utf-8')
+        print(f'Ответ от устройства: {a}')
+
+    except Exception as err:
+        print(err)
+        s.close()
+
+    time.sleep(2)
