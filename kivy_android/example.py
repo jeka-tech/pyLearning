0b75d34bb7a937c8ecd5bb13038ea8999a0ab2de
JT <jeka-tech@mail.ru>
2023-06-30 16:14:33 +0400

    Изменение фона layout

--- kivy_android/example.py
+++ kivy_android/example.py
@@ -1,15 +1,35 @@
+from threading import Thread
+from http.server import HTTPServer
+
+from kivy.graphics import Rectangle, Color
+
+
+import serv
+
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.widget import Widget
 from kivy.uix.camera import Camera
 from kivy.uix.boxlayout import BoxLayout
+
+
+class WhiteLayout(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        with self.canvas.before:
+            Color(1,1,1,1)
+            self.rect = Rectangle()
 class MyApp(App):
 
-    cam = Camera()
+    # cam = Camera()
     def build(self):
 
-        layout_1 = BoxLayout(orientation='vertical')
-        layout_1.add_widget(Button(text="Play", on_press = self.play_cam))
-        layout_1.add_widget(self.cam)
+        layout_1 = WhiteLayout(orientation='vertical', padding=50)
+        layout_1.add_widget(Button(text="Play", on_press = self.foo))
+        layout_1.add_widget(Label(text="Play"))
+
+        # layout_1.add_widget(self.cam)
 
         return layout_1
 
@@ -22,11 +42,33 @@ class MyApp(App):
         #     background_color = [1,1,0,1],
         #     background_normal = ""
         # )
-    def play_cam(self, inst):
-        self.cam.play = not self.cam.play
+    # def play_cam(self, inst):
+    #     self.cam.play = not self.cam.play
     def foo(self, inst):
         inst.text = "hi"
         print("test")
 
-if __name__ == '__main__':
+def serving():
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, serv.ServerHandler)
+    try:
+
+        httpd.serve_forever()
+
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+
+def gui():
+
     MyApp().run()
+
+t1 = Thread(target=serving)
+t2 = Thread(target=gui)
+
+if __name__ == '__main__':
+    # t1.start()
+    t2.start()
+    print("123")
+    # t1.join()
+    t2.join()
---
+++ kivy_android/serv.py
@@ -0,0 +1,61 @@
+from http.server import BaseHTTPRequestHandler
+
+
+class ServerHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # print("GET request, path:", self.path)
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write("html".encode('utf-8'))
+
+        if self.path == "/status":
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(self.path.encode('utf-8'))
+        else:
+            self.send_error(404, "Page Not Found {}".format(self.path))
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+
+        try:
+
+            if self.path == "/status":
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+
+            elif self.path == "/esp":
+
+                # outParams = json.loads(body.decode('utf-8'))
+                # temp = outParams.get('Temp01', 0)  # второй аргумент это дефолтное значение, если ключ не найден в словаре
+                # hum = outParams.get('Hum01', 0)  # второй аргумент это дефолтное значение, если ключ не найден в словаре
+
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                print("/esp")
+
+            else:
+                self.send_response(400, 'Bad Request: Method does not exist')
+                self.send_header('Content-Type', 'application/json')
+                self.end_headers()
+        except Exception as err:
+            print("do_POST exception: %s" % str(err))
+
+
+if __name__ == '__main__':
+    from http.server import HTTPServer
+
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, ServerHandler)
+    try:
+
+        httpd.serve_forever()
+
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
