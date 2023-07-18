08b62022d9de58cbcc7f06e959432a4b7bb03680
JT <jeka-tech@mail.ru>
2023-07-18 16:31:30 +0400

    Added TabbedPanelItem

--- kivy_android/test.py
+++ kivy_android/test.py
@@ -9,8 +9,127 @@ Builder.load_string("""
     
     TabbedPanelItem:
         text: 'Панель'
-        Label:
-            text: 'First tab content area'
+
+        GridLayout:
+            height: 1000
+            cols: 2
+
+            
+            BoxLayout: 
+                padding: 10
+                canvas.before:
+                    Color:
+                        rgba: 39/255, 204/255, 245/255, 1
+                    Line:
+                        width: 1
+                        rectangle: self.x, self.y, self.width, self.height
+                
+                GridLayout:
+                    cols: 4
+                    spacing: 10
+                    Label:
+                        text: 'Кабинет'
+                    Button:
+                        id: cab_1
+                        text: 'Свет 1'
+                    Button:
+                        id: cab_2
+                        text: 'Свет 2'
+                    Label:
+                    
+                    Label:
+                        text: 'Детская'
+                    Button:
+                        id: det_1
+                        text: 'Свет 1'
+                    Button:
+                        id: det_2
+                        text: 'Свет 2'
+                    Label:
+                    
+                    Label:
+                        text: 'Зал'
+                    Button:
+                        id: zal_1
+                        text: 'Свет 1'
+                    Button:
+                        id: zal_2
+                        text: 'Свет 2'
+                    Label:
+                    
+                    Label:
+                        text: 'Кухня'
+                    ToggleButton:
+                        id: kuh_1
+                        text: 'Свет 1'
+                    ToggleButton:
+                        id: kuh_2
+                        text: 'Свет 2'
+                    Label:
+                        id: lab1
+                        text: 'woooooh' if kuh_1.state == 'down' else ''
+                    
+                    Label:
+                        text: 'Ванная'
+                    ToggleButton:
+                        text: 'Свет'
+                    ToggleButton:
+                        text: 'Пол'
+                    ToggleButton:
+                        text: 'Вентилятор'
+                    
+                    Label:
+                        text: 'Туалет'
+                    ToggleButton:
+                        text: 'Свет'
+                    ToggleButton:
+                        text: 'Пол'
+                    ToggleButton:
+                        text: 'Вентилятор'
+                    
+                    Label:
+                        text: 'Коридор'
+                    ToggleButton:
+                        text: 'Свет'
+                    Label:
+                    Label:
+                    
+                    Label:
+                        text: 'Прачечная'
+                    ToggleButton:
+                        text: 'Свет'
+                    ToggleButton:
+                        text: 'Утюг'
+                    Label:
+                    
+            
+            BoxLayout:
+                size_hint: .6, 1
+                padding: 10
+                canvas.before:
+                    Color:
+                        rgba: 39/255, 204/255, 245/255, 1
+                    Line:
+                        width: 1
+                        rectangle: self.x, self.y, self.width, self.height
+                GridLayout:
+                    cols: 2
+                    Label:
+                        text: 'Температура\\n на улице'
+                    Label:
+                        text: '25.63 °С'
+                    
+                    Label:
+                        text: 'Влажность\\n на улице'
+                    Label:
+                        text: '35 %'
+                    
+                    Label:
+                        text: 'Атмосферное\\n давление'
+                    Label:
+                        text: '755 мм.рт.ст.'
+            
+                    
     TabbedPanelItem:
         text: 'Видео'
         BoxLayout:
@@ -41,6 +160,7 @@ class Test(TabbedPanel):
 class TabbedPanelApp(App):
     def build(self):
         return Test()
+
 
 
 if __name__ == '__main__':
